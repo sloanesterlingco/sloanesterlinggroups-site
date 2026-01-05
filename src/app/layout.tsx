f6f58dea05f2f52e6a7fb53c7e1127dbd6bd3830
Sloane Sterling <sloanesterlingco@gmail.com>
2026-01-05 11:23:36 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,7 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.png",
   },
 
   robots: {
